0bb20b90dd1de056484972731b10257801d17716
AnaS0f-0 <alopez501@soyudemedellin.edu.co>
2026-09-01 11:51:29 -0500

    Barra de vida

--- My unity/AGENTS.md
+++ My unity/AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: My unity
 - Unity version: Unity 2023.1.0f1
 - Active game object:
-  - Name: DinoPlayer
+  - Name: Cactus C (1)
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- My unity/Assets/_Main/Scripts/Runner/ObstaculoMortal.cs
+++ My unity/Assets/_Main/Scripts/Runner/ObstaculoMortal.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class ObstaculoMortal : MonoBehaviour
 {
+    [SerializeField] private PlayerStats _playerStats;
     private void OnCollisionEnter2D(Collision2D colision)
     {
         if (colision.gameObject.CompareTag("player"))
         {
-            Destroy(colision.gameObject);
+
+            _playerStats.RestarVida(10);
         }
     }
 }
